bdbcd2ededeec8ed5f049f263d1878ce9d71280c
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-05-03 23:12:14 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,17 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
+   def spiralize(size, n=1):
+    x = 1
+    counter = 0
+    incrt = 2
+    return_value = 0
+
+    while x <= size ** 2:
+        n += x
+        x += incrt
+        counter += 1
+        if counter == 4:
+            incrt += 2
+            counter = 0
+
     return_value = n
     return return_value
