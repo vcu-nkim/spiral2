3b8080390f07c36bad7897d89d2e859431fa3a4c
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-05-03 23:26:19 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,16 +1,15 @@
 def spiralize(size, n=1):
-    x = 1
-    counter = 0
-    incrt = 2
-    return_value = 0
-
-    while x <= size ** 2:
-        n += x
-        x += incrt
-        counter += 1
-        if counter == 4:
-            incrt += 2
-            counter = 0
-
+    spiral = [[1]*size for _ in xrange(size)]
+    def ok(y, x):
+        return y < size and x < size and y >= 0 and x >= 0 and spiral[y][x]
+    y, x, dy, dx = 1, -1, 0, 1
+    while ok(y + dy, x + dx):
+        if ok(y + 2*dy, x + 2*dx):
+            y += dy
+            x += dx
+        else:
+            dx, dy = dy*(2*dx-1), dx
+        spiral[y][x] = 0
+    return spiral
     return_value = n
     return return_value
